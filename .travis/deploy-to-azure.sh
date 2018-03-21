33821dddb63ebf1e89480ef06c7e6b45a44fd3ef
kheiakiyama <kheiakiyama@gmail.com>
2018-03-21 14:37:30 +0900

    update proxies.json

--- .travis/deploy-to-azure.sh
+++ .travis/deploy-to-azure.sh
@@ -42,3 +42,13 @@ dotnet ./azcopy/azcopy.dll --source ./_site --destination $BLOB_CONTENTS_URL --d
 dotnet ./azcopy/azcopy.dll --source ./_site --destination $BLOB_CONTENTS_URL --dest-key $STORAGE_KEY --recursive --quiet --include "*.svg" --set-content-type "image/svg+xml"
 dotnet ./azcopy/azcopy.dll --source ./_site --destination $BLOB_CONTENTS_URL --dest-key $STORAGE_KEY --recursive --quiet --include "*.jpg" --set-content-type "image/jpeg"
 dotnet ./azcopy/azcopy.dll --source ./_site --destination $BLOB_CONTENTS_URL --dest-key $STORAGE_KEY --recursive --quiet --include "*.png" --set-content-type "image/png"
+
+# update proxies.json
+mkdir $TRAVIS_BUILD_DIR/_function_proxy
+git clone https://$FUNCTION_GIT_USERNAME:$FUNCTION_GIT_PASSWORD@$FUNCTION_APP_NAME.scm.azurewebsites.net:443/$FUNCTION_APP_NAME.git $TRAVIS_BUILD_DIR/_function_proxy
+cd $TRAVIS_BUILD_DIR/.travis
+ruby ./make-jekyll-proxies.rb ../_site $TRAVIS_BUILD_DIR/_function_proxy
+ls -la $TRAVIS_BUILD_DIR/_function_proxy
+git add -A
+git commit -m "Build:$TRAVIS_BUILD_NUMBER Commit:$TRAVIS_COMMIT $TRAVIS_COMMIT_MESSAGE"
+git push origin master
